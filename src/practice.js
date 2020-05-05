09f46306d3e023a83459a1fc8c9ea9149f34b666
Logan Stribling <logan.stribling@gmail.com>
2020-05-05 11:59:50 -0600

    finished demo

---
+++ src/practice.js
@@ -0,0 +1,82 @@
+// console.log('hello practice');
+require('dotenv').config();
+const knex = require('knex');
+
+const knexInstance = knex({
+  client: 'pg',
+  connection: process.env.DB_URL
+});
+
+console.log('knex and driver installed correctly');
+
+// knexInstance
+//   .from('amazong_products')
+//   .select('product_id', 'name', 'price', 'category')
+//   .where({ name: 'Point of view gun' })
+//   .then(result => {
+//     console.log(result);
+//   });
+// searchByProduceName('holo')
+
+
+
+// const searchTerm = 'holo';
+
+// knexInstance
+//   .select('product_id', 'name', 'price', 'category')
+//   .from('amazong_products')
+//   .where('name', 'ILIKE', `%${searchTerm}%`)
+//   .then(result => {
+//     console.log(result);
+//   });
+
+
+// function paginateProducts(page) {
+//   const productsPerPage = 10;
+//   const offset = productsPerPage * (page - 1);
+//   knexInstance
+//     .select('product_id', 'name', 'price', 'category')
+//     .from('amazong_products')
+//     .limit(productsPerPage)
+//     .offset(offset)
+//     .then(result => {
+//       console.log(result);
+//     });
+// }
+
+// paginateProducts(2);
+
+
+// function getProductsWithImages() {
+//   knexInstance
+//     .select('product_id', 'name', 'price', 'category', 'image')
+//     .from('amazong_products')
+//     .whereNotNull('image')
+//     .then(result => {
+//       console.log(result);
+//     });
+// }
+
+// getProductsWithImages();
+
+function mostPopularVideosForDays(days) {
+  knexInstance
+    .select('video_name', 'region')
+    .count('date_viewed AS views')
+    .where(
+      'date_viewed',
+      '>',
+      knexInstance.raw(`now() - '?? days'::INTERVAL`, days)
+    )
+    .from('whopipe_video_views')
+    .groupBy('video_name', 'region')
+    .orderBy([
+      { column: 'region', order: 'ASC' },
+      { column: 'views', order: 'DESC' },
+    ])
+    .then(result => {
+      console.log(result);
+    });
+}
+
+mostPopularVideosForDays(30);
