8562e2cab2ed8caff919d4b8d50db9afbce34fef
Cesar Morales <cesarlenin2@live.com>
2020-05-06 09:54:28 -0700

    test all data is inserted

--- test/shopping-list-service.spec.js
+++ test/shopping-list-service.spec.js
@@ -6,24 +6,27 @@ describe('shopping-list-service object',() =>{
   const testList = [
     {
       name:'Fish tricks',
-      price: 13.10,
+      price: '13.10',
       category:'Main',
       checked: false,
-      date_added: new Date('2029-01-22T16:28:32.615Z')
+      date_added: new Date('2029-01-22T16:28:32.615Z'),
+      item_id: 1
     },
     {
       name:'Not Dogs',
-      price: 4.99,
+      price: '4.99',
       category:'Snack',
       checked: true,
-      date_added: new Date('2029-01-22T16:28:32.615Z')
+      date_added: new Date('2029-01-22T16:28:32.615Z'),
+      item_id: 2
     },
     {
       name:'Bluffalo Wings',
-      price: 5.50,
+      price: '5.50',
       category:'Snack',
       checked: false,
-      date_added: new Date('2029-01-22T16:28:32.615Z')
+      date_added: new Date('2029-01-22T16:28:32.615Z'),
+      item_id: 3
     }
   ];
 
@@ -41,5 +44,26 @@ describe('shopping-list-service object',() =>{
   afterEach(() => db('shopping_list').truncate());
 
   after(() => db.destroy());
+
+  describe('getAllShoppingList()',() => {
+
+ 
+    context('with data present', () => {
+      beforeEach('insert test list', () =>
+        db('shopping_list')
+          .insert(testList)
+      );
+
+      it('return all test list',()=>{
+        return ShoppingListService
+          .getAllShoppingList(db)
+          .then(items => expect(items).to.eql(testList));
+      });
+    });
+
+
+  });
+
+
 
 });
